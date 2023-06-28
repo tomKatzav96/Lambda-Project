ef1ec75d6d3f2345c465d4639a531b7b0bd24a73
tom.katzav <tom.katzav@gmail.com>
2023-06-28 12:50:26 +0300

    rename function dir and deploy file

--- deployment/deploy_get_info_lambda.sh
+++ deployment/deploy_get_info_lambda.sh
@@ -2,10 +2,10 @@
 
 
 echo "Package the dependencies"
-pip install --target ./wikipedia/package wikipedia-api boto3
-zip -r ./wikipedia/my-deployment-package.zip ./wikipedia/package/*
+pip install --target ./get_info/package wikipedia-api boto3
+zip -r ./get_info/my-deployment-package.zip ./get_info/package/*
 
-zip ./wikipedia/my-deployment-package.zip ./wikipedia/lambda_function.py
+zip ./get_info/my-deployment-package.zip ./get_info/lambda_function.py
 
 echo "Deploy the lambda function"
-aws lambda update-function-code --function-name get-info --zip-file fileb://wikipedia/my-deployment-package.zip
+aws lambda update-function-code --function-name get-info --zip-file fileb://get_info/my-deployment-package.zip
